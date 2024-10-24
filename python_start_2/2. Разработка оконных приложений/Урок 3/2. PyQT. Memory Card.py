f7205cd22ddfa6a493ddd6a7e7bde7902b0ffb90
DukeSpontaneous <eai_bat@tut.by>
2024-10-25 01:48:50 +0300

    Обновил примеры.

--- python_start_2/2. Разработка оконных приложений/Урок 3/2. PyQT. Memory Card.py
+++ python_start_2/2. Разработка оконных приложений/Урок 3/2. PyQT. Memory Card.py
@@ -1,4 +1,4 @@
-# создай приложение для запоминания информации
+# 1) Подключи нужные модули (QtCore и QtWidgets и их элементы).
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QApplication,
@@ -12,28 +12,25 @@ from PyQt5.QtWidgets import (
 )
 
 
+# 2) Создай объект-приложение, окно приложения. Задай заголовок и размеры.
 app = QApplication([])
 
 
 window = QWidget()
 window.setWindowTitle("Memory Card")
+window.resize(400, 200)
 
 
-"""Интерфейс приложения Memory Card"""
-btn_ok = QPushButton("Ответить")  # кнопка ответа
-lb_question = QLabel("Какой национальности не существует?")  # текст вопроса
+# 3) Создай виджет-вопрос и виджет-кнопку «Ответить».
+lb_question = QLabel("Какой национальности не существует?")
+btn_ok = QPushButton("Ответить")
 
 
-radio_group_box = QGroupBox("Варианты ответов")  # группа ответов (выделяется визуально)
-
-# радиальные переключатели с ответами
+# 4) Создай набор переключателей с вариантами ответов. Расположи их по лэйаутам и объедини в группу.
 rbtn_1 = QRadioButton("Энцы")
 rbtn_2 = QRadioButton("Смурфы")
 rbtn_3 = QRadioButton("Чулымцы")
 rbtn_4 = QRadioButton("Алеуты")
-
-# вертикальные будут внутри горизонтального
-layout_quest = QHBoxLayout()
 
 # два ответа в первый столбец
 layout_answer_column_1 = QVBoxLayout()
@@ -45,31 +42,30 @@ layout_answer_column_2 = QVBoxLayout()
 layout_answer_column_2.addWidget(rbtn_3)
 layout_answer_column_2.addWidget(rbtn_4)
 
-
+# вертикальные будут внутри горизонтального
+layout_quest = QHBoxLayout()
 layout_quest.addLayout(layout_answer_column_1)
-layout_quest.addLayout(layout_answer_column_2)  # разместили столбцы в одной строке
+layout_quest.addLayout(layout_answer_column_2)
+
+# группа ответов (выделяется визуально)
+radio_group_box = QGroupBox("Варианты ответов")
+radio_group_box.setLayout(layout_quest)
 
 
-radio_group_box.setLayout(layout_quest)  # готова "панель" с вариантами ответов
-
-
+# 5) Расположи вопрос, группу переключателей и кнопку по лэйаутам.
+# 6) При необходимости, добавь пробелы между виджетами и выровняй их по краю/центру.
 layout_line1 = QHBoxLayout()  # вопрос
 layout_line2 = QHBoxLayout()  # варианты ответов или результат теста
 layout_line3 = QHBoxLayout()  # кнопка "Ответить"
 
-
 layout_line1.addWidget(lb_question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
 layout_line2.addWidget(radio_group_box)
-
 
 layout_line3.addStretch(1)
 layout_line3.addWidget(btn_ok, stretch=2)  # кнопка должна быть большой
 layout_line3.addStretch(1)
 
-
-# Теперь созданные строки разместим друг под другой:
 layout_card = QVBoxLayout()
-
 
 layout_card.addLayout(layout_line1, stretch=2)
 layout_card.addLayout(layout_line2, stretch=8)
@@ -78,7 +74,8 @@ layout_card.addLayout(layout_line3, stretch=1)
 layout_card.addStretch(1)
 layout_card.setSpacing(5)  # пробелы между содержимым
 
-
 window.setLayout(layout_card)
+
+
 window.show()
 app.exec()
